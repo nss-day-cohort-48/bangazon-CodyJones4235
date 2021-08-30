f3deddac685bcab6d594df2c26bdb96412ea90e4
CodyJones4235 <codyjones4325@gmail.com>
2021-08-30 13:20:55 -0500

    Only responds with lineitems

--- bangazonapi/models/product.py
+++ bangazonapi/models/product.py
@@ -62,8 +62,8 @@ class Product(SafeDeleteModel):
         for rating in ratings:
             total_rating += rating.rating
 
-        avg = total_rating / len(ratings)
-        return avg
+        # avg = total_rating / len(ratings)
+        # return avg
 
     class Meta:
         verbose_name = ("product")
